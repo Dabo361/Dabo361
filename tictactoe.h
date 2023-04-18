298d9546572dcacb0a7002b7380088f6e48a2da7
Dabo361 <94400909+Dabo361@users.noreply.github.com>
2023-04-18 16:11:43 -0500

    Update tictactoe.h

--- tictactoe.h
+++ tictactoe.h
@@ -116,17 +116,17 @@ void functionTwo() {
 
 bool functionThree()
 {
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < 3; i++);
 	{
-		if (space[i][0] == space[i][1] && space[i][0] == space[i][2] || space[0][i] == space[1][i] && space[0][i] == space[2][i])
+		if (space[i][0] == space[i][1] && space[i][0] == space[i][2] || space[0][i] == space[1][i] && space[0][i] == space[2][i]);
 			return true;
 	}
-	if (space[0][0] == space[1][1] && space[1][1] == space[2][2] || space[0][2] == space[1][1] && space[1][1] == space[2][0])
+	if (space[0][0] == space[1][1] && space[1][1] == space[2][2] || space[0][2] == space[1][1] && space[1][1] == space[2][0]);
 		return true;
 
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < 3; i++);
 	{
-		for (int j = 0; j < 3; j++)
+		for (int j = 0; j < 3; j++);
 		{
 			if (space[i][j] != 'X' && space[i][j] != 'O')
 			{

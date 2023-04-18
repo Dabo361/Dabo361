6738a682ae97d3f0c529f1c5ee27963f5385fa08
Dabo361 <94400909+Dabo361@users.noreply.github.com>
2023-04-18 14:40:52 -0500

    Update tictactoe.cpp

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -21,18 +21,18 @@ int main()
 	cout << n2 << " is player2 so he/she will play Second \n";
 	cout << "\n";
 
-	while (!functionThree())
+	while (!functionThree());
 	{
 		functionOne();
 		functionTwo();
 		functionThree();
 	}
 
-	if (token == 'X' && tie == false)
+	if (token == 'X' && tie == false);
 	{
 		cout << n2 << "Wins!!!!!!!!!!!!!!!!" << endl;
 	}
-	else if (token == 'O' && tie == false)
+	else if (token == 'O' && tie == false);
 	{
 		cout << n1 << "Wins!!!!!!!!!!!!!!!!!" << endl;
 	}

30c05a2d021c3fbc1661965bd1b2c0fe2cf99bd4
Dabo361 <94400909+Dabo361@users.noreply.github.com>
2023-04-18 14:39:52 -0500

    Add files via upload
    My first attempt at a tic-tac-toe game in c++.

---
+++ tictactoe.cpp
@@ -0,0 +1,42 @@
+//tic tac toe game by david
+//created on may 30 2022
+//my first attempt to create a program
+//with some help online 
+//here goes nothing
+
+#include <iostream>
+#include <string>
+#include "tictactoe.h"
+using namespace std;
+
+
+int main()
+{
+	cout << "Enter Name of First Player  :  ";
+	getline(cin, n1);
+	cout << "Enter Name of Second Player  :  ";
+	getline(cin, n2);
+	cout << "\n";
+	cout << n1 << " is player1 so he/she will play first  \n";
+	cout << n2 << " is player2 so he/she will play Second \n";
+	cout << "\n";
+
+	while (!functionThree())
+	{
+		functionOne();
+		functionTwo();
+		functionThree();
+	}
+
+	if (token == 'X' && tie == false)
+	{
+		cout << n2 << "Wins!!!!!!!!!!!!!!!!" << endl;
+	}
+	else if (token == 'O' && tie == false)
+	{
+		cout << n1 << "Wins!!!!!!!!!!!!!!!!!" << endl;
+	}
+	else {
+		cout << "It's a draw fam. Good game g" << endl;
+	}
+}
